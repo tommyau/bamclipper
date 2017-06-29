e2e62c1f8215fe552d82e9e536ccb76ce18887a6
Tommy Au <chau@hksh.com>
2017-06-29 12:30:58 +0800

    new pipe mode

--- bamclipper.sh
+++ bamclipper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # bamclipper.sh
-VERSION=1.0.1
+VERSION=1.1.0
 NTHREAD=1
 SAMTOOLS="samtools"
 PARALLEL="parallel"
@@ -9,48 +9,56 @@ DOWNSTREAM=5
 SAMTOOLS_VERSION_REQUIRED=1.3.1
 PARALLEL_VERSION_REQUIRED=20130522
 
-function usage {
+
+# show usage if no option is provided
+if [[ "$#" -eq 0 ]]; then
     echo >&2 "Program: BAMClipper (Remove primer sequence from BAM alignments by soft-clipping)"
     echo >&2 "Version: $VERSION"
     echo >&2
+    echo >&2 "File mode"
     echo >&2 "Usage: $0 -b BAM -p BEDPE [-n NTHREAD] [-s SAMTOOLS] [-g GNUPARALLEL] [-u UPSTREAM] [-d DOWNSTREAM]"
-    echo >&2
     echo >&2 "Required arguments:"
     echo >&2 "    -b FILE    indexed BAM alignment file"
     echo >&2 "    -p FILE    BEDPE file of primer pair locations"
     echo >&2
-    echo >&2 "Options:"
+    echo >&2 "Pipe mode"
+    echo >&2 "Usage: bwa mem ref.fasta r1.fastq r2.fastq | $0 -i -p BEDPE [-n NTHREAD] [-s SAMTOOLS] [-g GNUPARALLEL] [-u UPSTREAM] [-d DOWNSTREAM] | ..."
+    echo >&2 "Required arguments:"
+    echo >&2 "    -i         read SAM alignment from STDIN"
+    echo >&2 "    -p FILE    BEDPE file of primer pair locations"
+    echo >&2
+    echo >&2 "Options for either mode:"
     echo >&2 "    -n INT     number of threads for clipprimer.pl and samtools sort [$NTHREAD]"
     echo >&2 "    -s FILE    path to samtools executable [$SAMTOOLS]"
     echo >&2 "    -g FILE    path to gnu parallel executable [$PARALLEL]"
     echo >&2 "    -u INT     number of nucleotide upstream to 5' most nucleotide of primer [$UPSTREAM]"
     echo >&2 "    -d INT     number of nucleotide downstream to 5' most nucleotide of primer [$DOWNSTREAM]"
     exit 1
+fi
+
+function error {
+    echo >&2 "ERROR: $1"
+    exit 1
 }
 
-
-while getopts ":b:p:n::s::g::u::d::" o; do
+while getopts ":ib:p:n:s:g:u:d:" o; do
     case "${o}" in
+	i)
+	    PIPE=1
+	    ;;
 	b)
 	    BAM=${OPTARG}
+	    [[ ! -f "$BAM" ]] && error "BAM file is not found ($BAM)"
+	    [[ ! -f "$BAM.bai" ]] && error "BAM file index $BAM.bai is not found. (Tips: $SAMTOOLS index $BAM)"
 	    BAMbn=$(basename "$BAM")
-	    if [[ ! -f "$BAM" ]]; then
-		echo >&2 "ERROR: BAM file $BAM cannot be found."
-		exit 1
-	    fi 
-	    if [[ ! -f "$BAM.bai" ]]; then
-		echo >&2 "ERROR: BAM file index $BAM.bai cannot be found."
-		echo >&2 "Tips: $SAMTOOLS index $BAM"
-		exit 1
-	    fi 
 	    ;;
 	p)
 	    BEDPE=${OPTARG}
-	    [[ -f "$BEDPE" ]] || usage
+	    [[ -f "$BEDPE" ]] || error "BEDPE file is not found ($BEDPE)"
 	    ;;
 	n)
 	    NTHREAD=${OPTARG}
-	    [[ "$NTHREAD" -ge 1 ]] || usage
+	    [[ "$NTHREAD" -ge 1 ]] || error "NTHREAD requires non-zero integer"
 	    ;;
 	s)
 	    SAMTOOLS=${OPTARG}
@@ -60,37 +68,42 @@ while getopts ":b:p:n::s::g::u::d::" o; do
 	    ;;
 	u)
 	    UPSTREAM=${OPTARG}
-	    [[ "$UPSTREAM" -ge 0 ]] || usage
+	    [[ "$UPSTREAM" =~ '^[0-9]+$' ]] || error "UPSTREAM requires non-negative integer"
+	    [[ "$UPSTREAM" -ge 0 ]] || error "UPSTREAM requires non-negative integer"
 	    ;;
 	d)
 	    DOWNSTREAM=${OPTARG}
-	    [[ "$DOWNSTREAM" -ge 0 ]] || usage
+	    [[ "$DOWNSTREAM" =~ '^[0-9]+$' ]] || error "DOWNSTREAM requires non-negative integer"
+	    [[ "$DOWNSTREAM" -ge 0 ]] || error "DOWNSTREAM requires non-negative integer"
 	    ;;
 	*)
-	    usage
+	    error "Invalid option: -$OPTARG"
 	    ;;
     esac
 done
 shift $((OPTIND-1))
 
-# assert: BAM and BEDPE are defined
-if [ -z "$BAM" ] || [ -z "$BEDPE" ]; then
-    usage
-fi
+# assert (either BAM file or PIPE mode, but not both) and BEDPE are defined
+([[ -z "$BAM" ]] && [[ -z "$PIPE" ]]) && error "BAM file (-b) or Pipe mode (-i) is not defined"
+([[ ! -z "$BAM" ]] && [[ "$PIPE" == 1 ]]) && error "File mode (-b) and Pipe mode (-i) cannot be defined simultaneously"
+[[ -z "$BEDPE" ]] && error "BEDPE file (-p) is not defined"
 
 # check parallel & version
-"$PARALLEL" --minversion $PARALLEL_VERSION_REQUIRED >/dev/null 2>&1 || { echo >&2 "ERROR: GNU Parallel (provided path: $PARALLEL) is not running properly. Please check the path and/or version (at least $PARALLEL_VERSION_REQUIRED)."; exit 1; }
-
-# check samtools & version
-function version { echo "$@" | cut -f1 -d"+" | awk -F. '{ printf("%03d%03d%03d\n", $1,$2,$3); }'; }
-"$SAMTOOLS" --version-only >/dev/null 2>&1 || { echo >&2 "ERROR: SAMtools (provided path: $SAMTOOLS) is not running properly. Please check the path and/or version (at least $SAMTOOLS_VERSION_REQUIRED)"; exit 1; }
-SAMTOOLS_VERSION=`"$SAMTOOLS" --version-only`
-if [ "$(version "$SAMTOOLS_VERSION")" -lt "$(version "$SAMTOOLS_VERSION_REQUIRED")" ]; then
-     echo >&2 "ERROR: SAMtools version ($SAMTOOLS_VERSION) is not supported (supported version: at least $SAMTOOLS_VERSION_REQUIRED)."
-     exit 1
-fi
+"$PARALLEL" --minversion $PARALLEL_VERSION_REQUIRED >/dev/null 2>&1 || error "GNU Parallel (provided path: $PARALLEL) is not running properly. Please check the path and/or version (at least $PARALLEL_VERSION_REQUIRED)."
 
 # run bamclipper
 SCRIPT_PATH="$(readlink -f $0)"
 SCRIPT_DIR="$(dirname $SCRIPT_PATH)"
-"$SAMTOOLS" collate -O --output-fmt SAM "$BAM" "${BAMbn}.sort1" | "$SCRIPT_DIR"/injectseparator.pl | "$PARALLEL" -j "$NTHREAD" --keep-order --remove-rec-sep --pipe --remove-rec-sep --recend '__\n' --block 1m "$SCRIPT_DIR/clipprimer.pl --in $BEDPE --upstream $UPSTREAM --downstream $DOWNSTREAM" | "$SAMTOOLS" sort -T "${BAMbn}.sort2" -@ "$NTHREAD" > "${BAMbn%.bam}.primerclipped.bam" && "$SAMTOOLS" index "${BAMbn%.bam}.primerclipped.bam"
+if [ "$PIPE" == 1 ]; then
+    # pipe mode
+    cat /dev/stdin | "$SCRIPT_DIR"/injectseparator.pl | "$PARALLEL" -j "$NTHREAD" --keep-order --remove-rec-sep --pipe --remove-rec-sep --recend '__\n' --block 10M "$SCRIPT_DIR/clipprimer.pl --in $BEDPE --upstream $UPSTREAM --downstream $DOWNSTREAM"
+else
+    # file mode
+    # check samtools & version
+    function version { echo "$@" | cut -f1 -d"+" | awk -F. '{ printf("%03d%03d%03d\n", $1,$2,$3); }'; }
+    "$SAMTOOLS" --version-only >/dev/null 2>&1 || error "SAMtools (provided path: $SAMTOOLS) is not running properly. Please check the path and/or version (at least $SAMTOOLS_VERSION_REQUIRED)"
+    SAMTOOLS_VERSION=`"$SAMTOOLS" --version-only`
+    [ "$(version "$SAMTOOLS_VERSION")" -lt "$(version "$SAMTOOLS_VERSION_REQUIRED")" ] && error "SAMtools version ($SAMTOOLS_VERSION) is not supported (supported version: at least $SAMTOOLS_VERSION_REQUIRED)."
+
+    "$SAMTOOLS" collate -O --output-fmt SAM "$BAM" "${BAMbn}.sort1" | "$SCRIPT_DIR"/injectseparator.pl | "$PARALLEL" -j "$NTHREAD" --keep-order --remove-rec-sep --pipe --remove-rec-sep --recend '__\n' --block 10M "$SCRIPT_DIR/clipprimer.pl --in $BEDPE --upstream $UPSTREAM --downstream $DOWNSTREAM" | "$SAMTOOLS" sort -T "${BAMbn}.sort2" -@ "$NTHREAD" > "${BAMbn%.bam}.primerclipped.bam" && "$SAMTOOLS" index "${BAMbn%.bam}.primerclipped.bam"
+fi
